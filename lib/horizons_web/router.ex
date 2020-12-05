04ee6ba579517e6a35c1347de4be1bceea8e4b36
BCrawfordScott <briancrawfordscott@gmail.com>
2020-12-05 15:14:43 -0600

    routers and endpoints

--- lib/horizons_web/router.ex
+++ lib/horizons_web/router.ex
@@ -10,6 +10,7 @@ defmodule HorizonsWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :fetch_current_user
+    plug Horizons.Plug.Locale, 'en'
   end
 
   pipeline :api do
@@ -20,6 +21,7 @@ defmodule HorizonsWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "users", UserConroller, only: [:index, :show]
   end
 
   # Other scopes may use custom stacks.
